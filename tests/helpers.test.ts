15c6a2e7e9758f3b62e9625b711e5a66cdd4c455
Simon Ashbery <si.ashbery@gmail.com>
2024-02-05 14:42:57 +0000

    typo

--- tests/helpers.test.ts
+++ tests/helpers.test.ts
@@ -10,7 +10,7 @@ const defaultFiles = [
         contents: 'content 2'
     },
     {
-        path: 'root/folde-2/file-3.js',
+        path: 'root/folder-2/file-3.js',
         contents: `content 3`
     },
 ]
@@ -23,7 +23,7 @@ describe(parseNodes, () => {
             { "name": "folder-1", "parent": "root" },
             { "contents": "content 1", "name": "file-1.js", "parent": "folder-1" },
             { "contents": "content 2", "name": "file-2.js", "parent": "folder-1" },
-            { "name": "folde-2", "parent": "root" },
+            { "name": "folder-2", "parent": "root" },
             { "contents": "content 3", "name": "file-3.js", "parent": "folde-2" }])
     })
 })
